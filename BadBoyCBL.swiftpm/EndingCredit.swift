716abc1c119f55cea9539c94be07115473d693e5
poodlepoodle <chammal97@naver.com>
2023-03-30 22:57:34 +0900

    :lipstick: style: 엔딩 크레딧 효과 추가

--- BadBoyCBL.swiftpm/EndingCredit.swift
+++ BadBoyCBL.swiftpm/EndingCredit.swift
@@ -8,26 +8,33 @@
 import SwiftUI
 
 struct EndingCredit: View {
+    @State var started = false;
+    
     var body: some View {
-        ZStack{
+        ZStack {
+            // Background Image
             Image("엔딩크레딧배경만")
                 .resizable()
-                .frame(maxWidth:.infinity, maxHeight: .infinity)
                 .scaledToFill()
-            HStack{
-                Image("엔딩크레딧이미지만")
-                    .resizable()
-                    .frame(width: 694,  height: 513)
-                    .scaledToFill()
-                    .offset(x: -320, y: 100)
+                .frame(width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height)
+                .edgesIgnoringSafeArea(.all)
+                
+            ZStack {
+                // Image that moves from bottom to top
                 Image("엔딩크레딧텍스트")
                     .resizable()
-                    .frame(width: 712,  height: 1862)
-                    .scaledToFill()
-                    .offset(x: -320, y: 100)
+                    .scaledToFit()
+                    .frame(width: 712, height: 1862)
+                    .offset(x: 330, y: started ? -600 : (UIScreen.main.bounds.height / 2) + 200)
+                    .onTapGesture {
+                        withAnimation(
+                            .linear(duration: 20)
+                        ) {
+                            started = true
+                        }
+                    }
             }
         }
-       
     }
 }
 
