d804f9fc598ff9897dd3c7ed073a55c13ba99c34
BGSMM <yoonbumtae@gmail.com>
2023-03-30 16:54:26 +0900

    Branch Commit Test 1

---
+++ BadBoyCBL.swiftpm/BranchTestView.swift
@@ -0,0 +1,20 @@
+//
+//  BranchTestView.swift
+//  BadBoyCBL
+//
+//  Created by 윤범태 on 2023/03/30.
+//
+
+import SwiftUI
+
+struct BranchTestView: View {
+    var body: some View {
+        Text("New Branch")
+    }
+}
+
+struct BranchTestView_Previews: PreviewProvider {
+    static var previews: some View {
+        BranchTestView()
+    }
+}
