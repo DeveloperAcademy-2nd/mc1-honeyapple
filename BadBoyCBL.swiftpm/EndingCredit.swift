cb9a95b652217ea81173af52469e5bbaa10eb501
earlysummer0303 <jw9914@naver.com>
2023-03-30 21:35:26 +0900

    에셋 업데이트 완료-2

--- BadBoyCBL.swiftpm/EndingCredit.swift
+++ BadBoyCBL.swiftpm/EndingCredit.swift
@@ -9,12 +9,32 @@ import SwiftUI
 
 struct EndingCredit: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Image("엔딩크레딧배경만")
+                .resizable()
+                .frame(maxWidth:.infinity, maxHeight: .infinity)
+                .scaledToFill()
+            HStack{
+                Image("엔딩크레딧이미지만")
+                    .resizable()
+                    .frame(width: 694,  height: 513)
+                    .scaledToFill()
+                    .offset(x: -320, y: 100)
+                Image("엔딩크레딧텍스트")
+                    .resizable()
+                    .frame(width: 712,  height: 1862)
+                    .scaledToFill()
+                    .offset(x: -320, y: 100)
+            }
+        }
+       
     }
 }
 
 struct EndingCredit_Previews: PreviewProvider {
     static var previews: some View {
         EndingCredit()
+            .previewInterfaceOrientation(.landscapeRight)
+            .previewDevice("iPad Pro (12.9-inch) (6th generation)")
     }
 }
--- BadBoyCBL.swiftpm/Outro2.swift
+++ BadBoyCBL.swiftpm/Outro2.swift
@@ -3,14 +3,22 @@ import SwiftUI
 struct Outro2: View {
     var body: some View {
         ZStack{
-            Image("Outro2")
+            //아웃트로2 뒷배경
+            Image("아웃트로2_배경최종")
                 .resizable()
                 .scaledToFill()
+            //아웃트로2 버디버디 화면
+            Image("아웃트로2_팝업")
+                .resizable()
+                .frame(width: 740.56, height: 516
+                )
+
+            //다음 버튼
             Button(action: {print("Button1")}){
-                Image("버튼")
+                Image("닫기버튼")
                     .resizable()
                     .frame(width: 99, height: 44.72)
-                    .offset(x: 408, y: 183)
+                    .offset(x: 282, y: 192)
             }
         }
     }
