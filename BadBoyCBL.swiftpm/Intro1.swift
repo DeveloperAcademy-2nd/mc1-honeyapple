ee69ad33cae5d7883e5bdfc331543f00341f4c54
BGSMM <yoonbumtae@gmail.com>
2023-03-30 21:44:18 +0900

    Assets 수정, 시뮬레이터 ContentView 대응

--- BadBoyCBL.swiftpm/Intro1.swift
+++ BadBoyCBL.swiftpm/Intro1.swift
@@ -1,20 +1,29 @@
 import SwiftUI
 
 struct Intro1: View {
+    @State var showIntro2 = false
+    
     var body: some View {
         ZStack{
             Image("뉴인트로")
                 .resizable()
                 .frame(maxWidth:.infinity, maxHeight: .infinity)
                 .scaledToFill()
-            Button(action: {print("Button1")}){
+            Button {
+                showIntro2 = true
+            } label: {
                 Image("버튼")
                    .resizable()
                    .frame(width: 178.01, height: 80.41)
-                   .offset(x: 550, y: 440)
-                   .padding(.bottom, 15)
+            }
+            .offset(x: 550, y: 440)
+            .padding(.bottom, 15)
+            
+            if showIntro2 {
+                Intro2()
             }
         }
+        
     }
 }
 
